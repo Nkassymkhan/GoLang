59dc3381e764166b7e043cbc65684b39e8e02b6f
Nkassymkhan <97424946+Nkassymkhan@users.noreply.github.com>
2023-01-30 17:39:22 +0600

    Add files via upload

---
+++ project/package/operations.go
@@ -0,0 +1,18 @@
+package operations
+
+func Sum(a int, b int) int{
+	return a + b
+}
+
+func Substraction(a int, b int) int{
+	return a - b
+}
+
+func Multiplicity(a int, b int) int{
+	return a * b
+}
+
+func Print(a int) int{
+	return a
+}
+
